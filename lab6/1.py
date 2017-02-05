47194c65e625473b6d781a79a6f066107a60a41c
notemissary <emissarys@protonmail.com>
2017-02-05 16:31:19 +0200

    Program descriptions.

--- lab6/1.py
+++ lab6/1.py
@@ -1,1 +1,7 @@
 # Dyma Volodymyr, KNIT16-A
+# По дате d, m, y определить дату следующего дня
+
+days = range(1, 32)
+months = range(1, 13)
+years = range(1901, 2016)
+d, m, y = int(input()), int(input()), int(input())
