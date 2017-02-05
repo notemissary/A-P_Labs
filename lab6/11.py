37ef21825fe59c44b67eb959c94865d824866117
notemissary <emissarys@protonmail.com>
2017-02-05 16:41:05 +0200

    Basic algorithm.

--- lab6/11.py
+++ lab6/11.py
@@ -5,3 +5,10 @@ days = range(1, 32)
 months = range(1, 13)
 years = range(1901, 2016)
 d, m, y = int(input()), int(input()), int(input())
+if d == 31:
+    if m == 12:
+        print('01/01', y + 1, sep='/')
+    else:
+        print('01', m + 1, y, sep='/')
+else:
+    print(d + 1, m, y, sep='/')
