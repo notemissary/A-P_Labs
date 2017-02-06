4174941fe48af57e580f0bb924138f700eee8ae9
notemissary <emissarys@protonmail.com>
2017-02-06 14:15:38 +0200

    + Refactored code

--- lab6/11.py
+++ lab6/11.py
@@ -3,60 +3,41 @@
 
 days = range(1, 32)
 months = range(1, 13)
-m30 = [4, 6, 9, 11]
 years = range(1901, 2017)
-feb = range(1, 29)
-leap_year = range(1900, 2017, 4)
-leap_feb = range(1, 30)
 
 while True:
     try:
         d, m, y = int(input('День: ')), int(input('Месяц: ')), \
                   int(input('Год: '))
-        if y in years and m in months and d in days:
-            if y in leap_year:
-                if m == 2:
-                    if d in leap_feb:
-                        if d == 29:
-                            print(1, m + 1, y, sep='/')
-                        else:
-                            print(d + 1, m, y, sep='/')
-                    else:
-                        print(y, '- високосный год. В феврале 29 дней.')
-                        continue
-            if m != 2:
-                if m in m30:
-                    if d == 30:
-                        if m == 12:
-                            print('1/1', y + 1, sep='/')
-                        else:
-                            print(1, m + 1, y, sep='/')
-                    else:
-                        print(d + 1, m, y, sep='/')
+        if d in days and m in months and y in years:
+            if m == 2 and (y % 4 == 0):
+                if d in range(1, 29):
+                    print('{}/{}/{}'.format(d + 1, m, y))
+                elif d == 29:
+                    print('{}/{}/{}'.format(1, m + 1, y))
                 else:
-                    if d == 30:
-                        if m == 12:
-                            print('1/1', y + 1, sep='/')
-                        else:
-                            print(1, m + 1, y, sep='/')
-                    else:
-                        print(d + 1, m, y, sep='/')
-            else:
-                if y not in leap_year:
-                    if d in feb:
-                        if d == 28:
-                            if m == 12:
-                                print('1/1', y + 1, sep='/')
-                            else:
-                                print(1, m + 1, y + 1, sep='/')
-                        else:
-                            print(d + 1, m, y, sep='/')
-                    else:
-                        print('В феврале невисокосного года 28 дней.')
-                        continue
+                    print('Out of range.')
+            elif m == 2:
+                if d in range(1, 28):
+                    print('{}/{}/{}'.format(d + 1, m, y))
+                elif d == 28:
+                    print('{}/{}/{}'.format(1, m + 1, y))
+                else:
+                    print('Out of range!')
+            elif m in {4, 6, 9, 11}:
+                if d in range(1, 30):
+                    print('{}/{}/{}'.format(d + 1, m, y))
+                elif d == 30:
+                    print('{}/{}/{}'.format(1, m + 1, y))
+                else:
+                    print('Out of range!')
+            elif m in {1, 3, 5, 7, 8, 10, 12}:
+                if d in range(1, 31):
+                    print('{}/{}/{}'.format(d + 1, m, y))
+                elif d == 31:
+                    print('{}/{}/{}'.format(1, m + 1, y))
         else:
-            print('Значения вне допустимого диапазона!')
-            continue
+            print('Out of range!')
     except ValueError:
         print('Значения должны быть целочисленными!')
         continue
