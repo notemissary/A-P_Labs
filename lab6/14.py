79859d4eb3bc466b96a780009c0a6995bb38478b
notemissary <emissarys@protonmail.com>
2017-02-06 11:42:16 +0200

    + Optimized calculations + Fixed input range

--- lab6/14.py
+++ lab6/14.py
@@ -13,22 +13,34 @@ animal = ['Rat', 'Cow', 'Tiger', 'Rabbit', 'Dragon', 'Snake', 'Horse',
           'Sheep', 'Monkey', 'Chicken', 'Dog', 'Pig']
 
 while True:
-    start = -56
+    start = 4
     flag = True
     try:
-        n = int(input('Input a year: '))
-        while flag:
-            for i in color:
-                for j in animal:
-                    if n == start:
-                        print(i, j)
-                        flag = False
+        year = int(input('Input a year: '))
+        if year == 0:
+            print(color[-1], animal[-4])
+        elif year == 1:
+            print(color[-1], animal[-3])
+        elif year == 2:
+            print(color[-1], animal[-2])
+        elif year == 3:
+            print(color[-1], animal[-1])
+        elif year in range(4, 2018):
+            while flag:
+                for i in color:
+                    for j in animal:
+                        if year == start:
+                            print(i, j)
+                            flag = False
+                            break
+                        start += 1
+                    if not flag:
                         break
-                    start += 1
-                if not flag:
-                    break
+        else:
+            print('Year is out of current era.')
+            continue
     except ValueError:
         print('The year must be an integer number!')
         continue
-    if input('Press Enter to continue or input something to exit') != '':
+    if input('Press Enter to continue or input something to exit...') != '':
         exit()
