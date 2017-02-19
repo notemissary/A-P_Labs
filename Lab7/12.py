83b65987b8b756834c1717ca593fc2037e483de1
notemissary <emissarys@protonmail.com>
2017-02-19 17:54:40 +0200

    + Какой-то супер-пупер алгоритм, но его понять бы ещё.

--- Lab7/12.py
+++ Lab7/12.py
@@ -1,1 +1,12 @@
 # Dyma Volodymyr. KNIT16-A
+# Написать программу вычислени суммы тех элементов матрицы a, номера строк и
+# столбцов которых принадлежат соответсвенно непутым множествам s_1 и s_2.
+
+n = 10
+a = [[1, 2, 3],
+     [4, 5, 6],
+     [7, 8, 9]]
+s_1 = {i for i in range(n)}
+s_2 = {i for i in range(n)}
+print(sum(val for row_id, row in enumerate(a) if row_id in s_1
+          for col_id, val in enumerate(row) if col_id in s_2))
