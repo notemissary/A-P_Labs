029b22841f73a1218b9758f6704b39eab97422af
notemissary <emissarys@protonmail.com>
2017-02-19 21:16:30 +0200

    + Release

--- Lab7/i_1a.py
+++ Lab7/i_1a.py
@@ -3,3 +3,23 @@
 # Вывести на экран все числа, которые входят в последовательность по одному
 # разу.
 
+while True:
+    try:
+        a = input('Input integers separated by space: ')
+        b = sorted(set(a.split()))
+        c = set()
+        if len(b) == 0:
+            print('Enter something!')
+            break
+        set(map(int, b))
+        for i in b:
+            if a.count(i) == 1:
+                c.add(i)
+        if len(c) == 0:
+            print('Result is an empty set.')
+        else:
+            print(c)
+    except ValueError:
+        print('Input numbers, separated by spaces!')
+    if input('Press Enter to continue...') != '':
+        break
