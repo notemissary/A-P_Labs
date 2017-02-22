52b55207c0ff4364da9e561565e2b495927cca13
notemissary <emissarys@protonmail.com>
2017-02-22 22:19:55 +0200

    + Release

--- Lab7/i_1b.py
+++ Lab7/i_1b.py
@@ -3,3 +3,32 @@
 # словами -- запятая, за последним словом -- точка. Вывести на экран в
 # алфавитном порядке все гласные буквы, которые входят в каждое слово.
 
+ru_alpha = {"а", "е", "ё", "о", "у", "э", "я", "ю", "ы", "и"}
+while True:
+    inp = input()
+    if inp.endswith('.'):
+        inp = set(inp.split(', '))
+        if '.' in inp:
+            inp.remove('.')
+        res = set()
+        t = None
+        for i in ru_alpha:
+            c = 0
+            flag = False
+            for j in inp:
+                temp = set(j)
+                if i in temp:
+                    c += 1
+                    flag = True
+                    continue
+                break
+            if flag and c == len(inp):
+                res.add(i)
+                continue
+        if len(res) > 0:
+            print(sorted(res))
+        else:
+            print('Результат - пустое множество. '
+                  'Возможно, ошибка ввода.')
+    else:
+        print('Последовательность должна заканчиваться точкой.')
