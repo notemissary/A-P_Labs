7e29a69fab2281aaf45bcff4ccb2fabb4091d1eb
notemissary <emissarys@protonmail.com>
2017-03-30 00:39:12 +0300

    .

--- Lab8/i3.2(3).py
+++ Lab8/i3.2(3).py
@@ -8,7 +8,7 @@ W = input('Введите подстроку: ')
 m = len(S)
 n = len(W)
 
-d = np.zeros((m, n))
+d = np.zeros((m, n), int)
 for i in range(0, m):
     d[i, 0] = i
 for j in range(0, n):
@@ -22,4 +22,4 @@ for j in range(1, n):
             d[i, j] = min([d[i-1, j]+1,
                            d[i, j-1]+1,
                            d[i-1, j-1]+1])
-print(int(d[m-1, n-1]))
+print(d[m-1, n-1])
