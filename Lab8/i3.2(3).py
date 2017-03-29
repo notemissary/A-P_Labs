0e9606f9551edacf7100c2505e1cd06d0909eb09
notemissary <emissarys@protonmail.com>
2017-03-30 00:48:25 +0300

    .

--- Lab8/i3.2(3).py
+++ Lab8/i3.2(3).py
@@ -9,17 +9,22 @@ m = len(S)
 n = len(W)
 
 d = np.zeros((m, n), int)
+
+# the distance of any first string to an empty second string
+# (transforming the string of the first i characters of S into
+# the empty string requires i deletions)
 for i in range(0, m):
     d[i, 0] = i
 for j in range(0, n):
-    d[0, j] = j
+    d[0, j] = j  # the distance of any second string to an empty first string
 
 for j in range(1, n):
     for i in range(1, m):
         if S[i] == W[j]:
             d[i, j] = d[i-1, j-1]
         else:
-            d[i, j] = min((d[i-1, j]+1,
-                           d[i, j-1]+1,
-                           d[i-1, j-1]+1))
+            d[i, j] = min((d[i-1, j]+1,  # a deletion
+                           d[i, j-1]+1,  # an insertion
+                           d[i-1, j-1]+1))  # a substitution
+
 print(d[m-1, n-1])
