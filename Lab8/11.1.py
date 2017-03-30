641ebd850fa02ae5059b0394a83afee77061a4be
notemissary <emissarys@protonmail.com>
2017-03-30 22:27:33 +0300

    .

--- Lab8/11.1.py
+++ Lab8/11.1.py
@@ -6,3 +6,5 @@ m = np.array([[1, 2, 3],
               [4, 5, 6],
               [7, 8, 9]], int)
 print(np.transpose(np.flip(m, 0), (1, 0)))
+
+
--- Lab8/11.2.py
+++ Lab8/11.2.py
@@ -3,7 +3,24 @@
 # (без выполнения лишних сдвигов)
 
 import numpy as np
+import itertools
 
 K = int(input('Насколько сдвинуть массив? '))
-m = np.arange(0, 10)
-print(m, 'Shifting...', np.roll(m, K), sep="\n\n")
+m = np.arange(0, 10).ravel()
+b = np.broadcast(K, 0)
+s = {ax: 0 for ax in range(m.ndim)}
+for sh, ax in b:
+    if -m.ndim <= ax < m.ndim:
+        s[ax % m.ndim] += sh
+rolls = [((slice(None), slice(None)),)] * m.ndim
+res = np.empty_like(m)
+for ax, offset in s.items():
+    offset %= m.shape[ax] or 1
+    if offset:
+        rolls[ax] = ((slice(None, -offset), slice(offset, None)),
+                     (slice(-offset, None), slice(None, offset)))
+    for indicies in itertools.product(*rolls):
+        arr_index, res_index = zip(*indicies)
+        res[res_index] = m[arr_index]
+
+print(res)
