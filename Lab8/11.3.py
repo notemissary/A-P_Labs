16be759c926d6f9cd490294e40695bf6dba5abad
notemissary <emissarys@protonmail.com>
2017-03-31 01:30:48 +0300

    .

--- Lab8/11.3.py
+++ Lab8/11.3.py
@@ -3,6 +3,17 @@
 
 import numpy as np
 
-m = np.array([[2, 3], [4, 5]])
-n = np.array([[5, 4], [3, 2]])
-print(np.dot(m, n))
+m = np.ones((3, 3))
+n = np.ones((3, 3))
+for i in range(3):
+    for j in range(3):
+        m[i][j] = int(input('Введите элемент m[{}][{}]: '.format(i+1, j+1)))
+for i in range(3):
+    for j in range(3):
+        n[i][j] = int(input('Введите элемент n[{}][{}]: '.format(i+1, j+1)))
+res = np.zeros((3, 3))
+for i in range(3):
+    for k in range(3):
+        for u in range(3):
+            res[i, k] += m[i][u] * n[u][k]
+print(res)
