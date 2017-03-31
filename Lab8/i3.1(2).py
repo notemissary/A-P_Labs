e09ec238d2cec39703adca9d175c37b900316276
notemissary <emissarys@protonmail.com>
2017-03-31 08:55:01 +0300

    .

--- Lab8/i3.1(2).py
+++ Lab8/i3.1(2).py
@@ -3,23 +3,17 @@
 
 import numpy as np
 
-n = int(input('Введите кол-во элементов массива: '))
-A = np.arange(n+1)
-T = int(input('Введите искомый элемент: '))
-L = 0
-R = n
-while True:
-    if L > R:
-        print('Неудача! Элемент {} не найден.'.format(T))
-        break
+r = int(input('Введите кол-во элементов массива: '))
+A = np.arange(r+1)
+l = int(input('Введите искомый элемент: '))
+while r - l > 1:
+    m = (l+r)
+    if l < A[m]:
+        r = m
     else:
-        m = int((L + R)/2)
-        if A[m] < T:
-            L = m + 1
-            continue
-        elif A[m] > T:
-            R = m - 1
-            continue
-        else:
-            print('Успех! Элемент {} найден на позиции {}.'.format(T, m+1))
-            break
+        l = m
+if A[l] == l:
+    print('Элемент найден на позиции {}'.format(l))
+else:
+    print('Элемент не найден.')
+
