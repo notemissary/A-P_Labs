b14b741a4416a9be39a1670f378eef939f514fdf
notemissary <emissarys@protonmail.com>
2017-04-02 23:02:06 +0300

    .

--- Lab8/11.4.py
+++ Lab8/11.4.py
@@ -2,12 +2,32 @@
 # Вычислите определитель квадратной матрицы (массива)
 
 import numpy as np
+import random as r
 
-e = np.ones((3, 3))
-for i in range(3):
-    for j in range(3):
-        e[i][j] = float(input('Введите элемент e[{}][{}]: '.format(i+1, j+1)))
-det = e[0][0] * e[1][1] * e[2][2] + e[0][1] * e[1][2] * e[2][0] + \
-      e[0][2] * e[1][0] * e[2][1] - e[0][2] * e[1][1] * e[2][0] - \
-      e[0][0] * e[1][2] * e[2][1] - e[0][1] * e[1][0] * e[2][2]
-print(det)
+
+def det(matrix, mul=1):
+    width = len(matrix)
+    if width == 1:
+        return mul * matrix[0][0]
+    else:
+        sign = -1
+        total = 0
+        for i in range(width):
+            n = []
+            for j in range(1, width):
+                buff = []
+                for k in range(width):
+                    if k != i:
+                        buff.append(matrix[j][k])
+                n.append(buff)
+            sign *= -1
+            total += mul * det(n, sign * matrix[0][i])
+    return total
+
+N = int(input('Введите размер квадратной матрицы: '))
+m = np.arange(N*N)
+for i in range(len(m)):
+    m[i] = r.randint(-10, 10)
+m = m.reshape(N, N)
+print(m)
+print(det(m))
