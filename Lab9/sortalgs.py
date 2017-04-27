59bd0be2c5733f3e532f2bf2e2218bd7a4776f48
notemissary <emissarys@protonmail.com>
2017-04-27 12:32:56 +0300

    Changed like everything

--- Lab9/sortalgs.py
+++ Lab9/sortalgs.py
@@ -15,13 +15,18 @@ def bubble(arr):
     they are in the wrong order.
     
     :param arr: Takes an array.
-    :return: Returns sorted array.
+    :return: Returns sorted array, comparision and switch number.
     """
+    c = 0
+    s = 0
     for j in range(len(arr)-1, 0, -1):
-        for i in range(len(arr)-1):
+        for i in range(len(arr)-1-c):
             if arr[i] > arr[i+1]:
                 arr[i], arr[i+1] = arr[i+1], arr[i]
-    return arr
+                s += 1
+            c += 1
+        c += 1
+    return arr, c, s
 
 
 def selection(arr):
@@ -30,13 +35,17 @@ def selection(arr):
     It is a sorting algorithm, specifically an in-place comparison sort.
     
     :param arr: Takes an array.
-    :return: Returns sorted array.
+    :return: Returns sorted array, comparsision number and switches number
     """
+    s = 0
+    c = 0
     for i in range(len(arr)):
         m = min(arr[i:])
         mi = where(arr[i:] == m)[0][0]
         arr[i+mi], arr[i] = arr[i], m
-    return arr
+        s += 1
+        c += 1
+    return arr, c, s
 
 
 def insertion(arr):
@@ -46,16 +55,21 @@ def insertion(arr):
     at a time.
     
     :param arr: Takes an array.
-    :return: Returns sorted array.
+    :return: Returns sorted array, comparision number and switch number.
     """
+    c = 2
+    s = 0
     for i in range(1, len(arr)):
         x = arr[i]
         j = i - 1
         while j >= 0 and arr[j] > x:
             arr[j+1] = arr[j]
             j -= 1
+            c += 2
         arr[j+1] = x
-    return arr
+        s += 1
+        c += 1
+    return arr, c, s
 
 
 def shake(arr):
@@ -65,31 +79,29 @@ def shake(arr):
     and a comparison sort.
 
     :param arr: Takes an array.
-    :return: Returns sorted array and comparision number.
+    :return: Returns sorted array, comparision number and switch number.
     """
-    c = 0
+    c = 3
+    s = 0
     left = 0
     right = len(arr) - 1
     while left <= right:
         for j in range(left, right):
+            c += 2
             if arr[j] > arr[j + 1]:
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
-                c += 1
-            else:
-                c += 1
+                s += 1
         right -= 1
 
         for j in range(right, left, -1):
+            c += 2
             if arr[j - 1] > arr[j]:
                 arr[j - 1], arr[j] = arr[j], arr[j - 1]
-                c += 1
-            else:
-                c += 1
+                s += 1
         left += 1
+
         c += 1
-    else:
-        c += 1
-    return arr, c
+    return arr, c, s
 
 
 def shell(arr):
@@ -98,25 +110,32 @@ def shell(arr):
     It is an in-place comparison sort.
 
     :param arr: Takes an array.
-    :return: Returns sorted array and comparision number.
+    :return: Returns sorted array, comparision number and switch number.
     """
-    c = 0
+    c = 2
+    s = 0
     gap = len(arr) // 2
     while gap > 0:
         for i in range(gap, len(arr)):
             val, j = arr[i], i
+            s += 1
             while j >= gap and arr[j - gap] > val:
                 arr[j] = arr[j - gap]
                 j -= gap
                 c += 2
-            else:
-                c += 1
             arr[j] = val
         gap //= 2
         c += 1
-    else:
-        c += 1
-    return arr, c
+    # gaps = [701, 301, 132, 57, 23, 10, 4, 1]
+    # for gap in gaps:
+    #     for i in range(gap, arr[-1]):
+    #         temp = arr[i]
+    #         j = i
+    #         while j >= gap and arr[j-gap] > temp:
+    #             arr[j] = arr[j-gap]
+    #             j -= gap
+    #         arr[j] = temp
+    return arr, c, s
 
 
 def heap(arr):
@@ -131,43 +150,36 @@ def heap(arr):
     :param arr: Takes an array.
     :return: Returns sorted array and comparision number
     """
-    c = 0
+    c = 3
+    s = 0
 
     def siftdown(array, start, ending):
-        nonlocal c
+        nonlocal c, s
         root = start
         while 2 * root + 1 <= ending:
+            c += 4
             child = 2 * root + 1
             swap = root
             if array[swap] < array[child]:
                 swap = child
-                c += 1
-            else:
-                c += 1
             if child + 1 <= ending and array[swap] < array[child + 1]:
                 swap = child + 1
-                c += 2
-            else:
-                c += 1
             if swap == root:
                 c += 1
                 return array
             else:
                 array[root], array[swap] = array[swap], array[root]
+                s += 1
                 root = swap
                 c += 1
             c += 1
-        else:
-            c += 1
 
     def heapify(array, arraylen):
-        nonlocal c
+        nonlocal c, s
         start = (arraylen - 2) // 2
         while start >= 0:
             siftdown(array, start, arraylen - 1)
             start -= 1
-            c += 1
-        else:
             c += 1
         return array
 
@@ -176,10 +188,9 @@ def heap(arr):
     end = arrlen - 1
     while end > 0:
         arr[end], arr[0] = arr[0], arr[end]
+        s += 1
         end -= 1
         siftdown(arr, 0, end)
-        c += 1
-    else:
         c += 1
     return arr, c
 
