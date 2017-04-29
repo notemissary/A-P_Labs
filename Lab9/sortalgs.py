3187375ae983391ee7e8d7b854fc23ed086d68a9
notemissary <emissarys@protonmail.com>
2017-04-29 20:07:43 +0300

    Some comments were added. Changed shellsort docstring.

--- Lab9/sortalgs.py
+++ Lab9/sortalgs.py
@@ -2,9 +2,14 @@
 # Bubble sort, selection sort, insertion sort,
 # Cocktail shaker sort, Shellsort, Heapsort
 
-from numpy import where, asarray, set_printoptions, empty
-from random import sample
-from time import clock
+from numpy import where, asarray, set_printoptions, empty, random
+from time import time
+from copy import deepcopy
+from gc import collect, enable
+
+print('Enabling garbage collector...', end=' ')
+enable()
+print('DONE!\nInitializing functions...', end=' ')
 
 
 def bubble(arr):
@@ -18,15 +23,17 @@ def bubble(arr):
     :return: Returns sorted array, comparision and switch number.
     """
     c = 0
-    s = 0
+    e = 0
+    h = 0
     for j in range(len(arr)-1, 0, -1):
-        for i in range(len(arr)-1-c):
+        for i in range(len(arr)-1-h):
             if arr[i] > arr[i+1]:
                 arr[i], arr[i+1] = arr[i+1], arr[i]
-                s += 1
-            c += 1
+                e += 1
+            c += 2
         c += 1
-    return arr, c, s
+        h += 1
+    return arr, c, e
 
 
 def selection(arr):
@@ -37,15 +44,15 @@ def selection(arr):
     :param arr: Takes an array.
     :return: Returns sorted array, comparsision number and switches number
     """
-    s = 0
+    e = 0
     c = 0
     for i in range(len(arr)):
         m = min(arr[i:])
         mi = where(arr[i:] == m)[0][0]
         arr[i+mi], arr[i] = arr[i], m
-        s += 1
+        e += 1
         c += 1
-    return arr, c, s
+    return arr, c, e
 
 
 def insertion(arr):
@@ -58,7 +65,7 @@ def insertion(arr):
     :return: Returns sorted array, comparision number and switch number.
     """
     c = 2
-    s = 0
+    e = 0
     for i in range(1, len(arr)):
         x = arr[i]
         j = i - 1
@@ -67,9 +74,9 @@ def insertion(arr):
             j -= 1
             c += 2
         arr[j+1] = x
-        s += 1
+        e += 1
         c += 1
-    return arr, c, s
+    return arr, c, e
 
 
 def shake(arr):
@@ -81,8 +88,8 @@ def shake(arr):
     :param arr: Takes an array.
     :return: Returns sorted array, comparision number and switch number.
     """
-    c = 3
-    s = 0
+    c = 1
+    e = 0
     left = 0
     right = len(arr) - 1
     while left <= right:
@@ -90,18 +97,42 @@ def shake(arr):
             c += 2
             if arr[j] > arr[j + 1]:
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
-                s += 1
+                e += 1
         right -= 1
 
         for j in range(right, left, -1):
             c += 2
             if arr[j - 1] > arr[j]:
                 arr[j - 1], arr[j] = arr[j], arr[j - 1]
-                s += 1
+                e += 1
         left += 1
 
         c += 1
-    return arr, c, s
+    return arr, c, e
+
+
+def shellprep(lenarr):
+    if lenarr > 4000:
+        # Robert Sedgewick's gap sequence (1986)
+        # Formulas:
+        # a(n) = 9*2^n - 9*2^(n/2) + 1 if n is even;
+        # a(n) = 8*2^n - 6*2^((n+1)/2) + 1 if n is odd.
+        # G.f.: (8*x^4 + 2*x^3 - 8*x^2 - 4*x - 1)/((x-1)*(2*x+1)*(2*x-1)*(2*x^2-1))
+        g = asarray((4294770689, 2415771649, 1073643521, 603906049, 268386305,
+                     150958081, 67084289, 37730305, 16764929, 9427969,
+                     4188161, 2354689, 1045505, 587521, 260609, 146305,
+                     64769, 36289, 16001, 8929, 3905, 2161, 929, 505, 209,
+                     109, 41, 19, 5, 1))
+    else:
+        # Marcin Ciura's gap sequence (2001)
+        # Experimentaly derived. Best for less than 4000 elements arrays
+        g = asarray((1750, 701, 301, 132, 57, 23, 10, 4, 1))
+    w = 0
+    for gg in g:
+        if lenarr >= gg:
+            w = where(g == gg)[0][0]
+            break
+    return g[w:]
 
 
 def shell(arr, gaps):
@@ -113,11 +144,11 @@ def shell(arr, gaps):
     Both sequences are the best-known for such lengths.
 
     :param arr: Takes an array.
-    :param gaps: Takes gaps sequence.
+    :param gaps: Takes a gaps sequence.
     :return: Returns sorted array, comparision number and switch number.
     """
     c = 2
-    s = 0
+    e = 0
     for gap in gaps:
         for i in range(gap, len(arr)):
             val = arr[i]
@@ -127,10 +158,10 @@ def shell(arr, gaps):
                 j -= gap
                 c += 2
             arr[j] = val
-            s += 1
+            e += 1
             c += 1
         c += 1
-    return arr, c, s
+    return arr, c, e
 
 
 def heap(arr):
@@ -146,32 +177,30 @@ def heap(arr):
     :return: Returns sorted array and comparision number
     """
     c = 3
-    s = 0
+    e = 0
 
     def siftdown(array, start, ending):
-        nonlocal c, s
+        nonlocal c, e
         root = start
-        while 2 * root + 1 <= ending:
+        child = (root << 1) + 1
+        while child <= ending:
             c += 4
-            child = 2 * root + 1
             swap = root
             if array[swap] < array[child]:
                 swap = child
             if child + 1 <= ending and array[swap] < array[child + 1]:
                 swap = child + 1
             if swap == root:
-                c += 1
                 return array
             else:
                 array[root], array[swap] = array[swap], array[root]
-                s += 1
+                e += 1
                 root = swap
-                c += 1
-            c += 1
+            child = (root << 1) + 1
 
     def heapify(array, arraylen):
-        nonlocal c, s
-        start = (arraylen - 2) // 2
+        nonlocal c
+        start = (arraylen - 2) >> 1
         while start >= 0:
             siftdown(array, start, arraylen - 1)
             start -= 1
@@ -183,22 +212,25 @@ def heap(arr):
     end = arrlen - 1
     while end > 0:
         arr[end], arr[0] = arr[0], arr[end]
-        s += 1
+        e += 1
         end -= 1
         siftdown(arr, 0, end)
         c += 1
-    return arr, c
+    return arr, c, e
 
 
 def randarr(num):
     try:
-        return asarray(sample(range(-2 * num, 2 * num), num))
+        return random.permutation(num)
     except (MemoryError, OverflowError):
-        print('Error! Not enough memory!')
         return None
 
+print('DONE!\nReserving variables...', end=' ')
+res, gp = None, None
+print('DONE!\nSetting up print options...', end=' ')
+set_printoptions(threshold=10, edgeitems=4)
 
-set_printoptions(threshold=10)
+print('DONE!\n\n\nWelcome to SortAlgs!')
 while True:
     choice = input('Would you want to fill the array yourself? [y/n]\n> '
                    '').lower()
@@ -228,95 +260,187 @@ while True:
         while True:
             try:
                 integer = int(input('Input length of the array: '))
-                if integer > 0:
+                if 0 < integer <= 50000000:
+                    print('Generating the array...', end=' ')
                     A = randarr(integer)
                     if A is None:
+                        print('Error! Not enough memory!')
                         continue
+                    print('DONE!')
+                    break
+                elif integer > 50000000:
+                    print('Error! Array is too long for this machine.')
                 else:
                     print('Error! Length MUST be positive number.')
-                    continue
             except ValueError:
                 print('Error! Length must be integer!')
-            break
     else:
         print('Error! Answer with y for yes and n for no')
         continue
-    lenA = len(A)
-    if lenA > 4000:
-        # Robert Sedgewick's gap sequence (1986)
-        # Formulas:
-        # a(n) = 9*2^n - 9*2^(n/2) + 1 if n is even;
-        # a(n) = 8*2^n - 6*2^((n+1)/2) + 1 if n is odd.
-        # G.f.: (8*x^4 + 2*x^3 - 8*x^2 - 4*x - 1)/((x-1)*(2*x+1)*(2*x-1)*(2*x^2-1))
-        g = asarray((4294770689, 2415771649, 1073643521, 603906049, 268386305,
-                     150958081, 67084289, 37730305, 16764929, 9427969,
-                     4188161, 2354689, 1045505, 587521, 260609, 146305,
-                     64769, 36289, 16001, 8929, 3905, 2161, 929, 505, 209,
-                     109, 41, 19, 5, 1))
-    else:
-        # Marcin Ciura's gap sequence (2001)
-        # Experimentaly derived. Best for less than 4000 elements arrays
-        g = asarray((1750, 701, 301, 132, 57, 23, 10, 4, 1))
-    w = 0
-    for gg in g:
-        temp = gg
-        if lenA >= gg:
-            w = where(g == temp)[0][0]
-            print(w)
-            break
-    g = g[w:]
-    print(g)
     while True:
-        ch = input('What sort would you want to use?\n'
+        ch = input('What sorting algorithm would you want to use?\n'
                    '1 - Bubble sort\n'
                    '2 - Selsection sort\n'
                    '3 - Insertion sort\n'
                    '4 - Cocktail shaker sort\n'
                    '5 - Shellsort\n'
                    '6 - Heapsort\n'
+                   '9 - TEST \'EM ALL!\n'
                    '> ').lower()
         if ch == '1':
             print('Original array: \n{}'.format(A))
-            print('Bubble sort: \n{}\n'
+            t = time()
+            res = bubble(A)
+            t = time() - t
+            print('Bubble sort: \nArray: \n{}\n'
+                  'Comparisions: {} | Exchanges: {}\n'
+                  'Time: {:.6f} seconds'
                   '====================================================='
-                  ''.format(bubble(A)))
+                  ''.format(res[0], res[1], res[2], t))
         elif ch == '2':
             print('Original array: \n{}'.format(A))
-            print('Selection sort: \n{}\n'
+            t = time()
+            res = selection(A)
+            t = time() - t
+            print('Selection sort: \nArray: \n{}\n'
+                  'Comparisions: {} | Exchanges: {}\n'
+                  'Time: {:.6f} seconds'
                   '====================================================='
-                  ''.format(selection(A)))
+                  ''.format(res[0], res[1], res[2], t))
         elif ch == '3':
             print('Original array: \n{}'.format(A))
-            print('Insertion sort: \n{}\n'
+            t = time()
+            res = insertion(A)
+            t = time() - t
+            print('Insertion sort: \nArray: \n{}\n'
+                  'Comparisions: {} | Exchanges: {}\n'
+                  'Time: {:.6f} seconds'
                   '====================================================='
-                  ''.format(insertion(A)))
+                  ''.format(res[0], res[1], res[2], t))
         elif ch == '4':
             print('Original array: \n{}'.format(A))
-            t = clock()
+            t = time()
             res = shake(A)
-            t = clock() - t
-            print('Cocktail shaker sort: \n{}\nComparision number: {}\n'
+            t = time() - t
+            print('Cocktail shaker sort: \nArray: \n{}\n'
+                  'Comparisions: {} | Exchanges: {}\n'
                   'Time: {:.6f} seconds\n'
                   '====================================================='
-                  ''.format(res[0], res[1], t))
+                  ''.format(res[0], res[1], res[2], t))
         elif ch == '5':
+            gp = shellprep(len(A))
             print('Original array: \n{}'.format(A))
-            t = clock()
-            res = shell(A, g)
-            t = clock() - t
-            print('Shellsort: \n{}\nComparision number: {}\n'
+            t = time()
+            res = shell(A, gp)
+            t = time() - t
+            print('Shellsort: \nArray: \n{}\n'
+                  'Comparisions: {} | Exchanges: {}\n'
                   'Time: {:.6f} seconds\n'
                   '====================================================='
-                  ''.format(res[0], res[1], t))
+                  ''.format(res[0], res[1], res[2], t))
         elif ch == '6':
             print('Original array: \n{}'.format(A))
-            t = clock()
+            t = time()
             res = heap(A)
-            t = clock() - t
-            print('Heapsort: \n{}\nComparision number: {}\n'
+            t = time() - t
+            print('Heapsort: \nArray: \n{}\n'
+                  'Comparisions: {} | Exchanges: {}\n'
                   'Time: {:.6f} seconds\n'
                   '====================================================='
-                  ''.format(res[0], res[1], t))
+                  ''.format(res[0], res[1], res[2], t))
+        elif ch == '9':
+            print('Original array: \n{}'.format(A))
+            print('Woah! FIRE!!!')
+            print('Collecting garbage...', end=' ')
+            del res, gp
+            collect()
+            print('DONE!\n\nBubble sort')
+            t = time()
+            res = bubble(deepcopy(A))
+            t = time() - t
+            print('=' * 75)
+            print('Array: \n{}\n'
+                  'Comparisions: {} | Exchanges: {}\n'
+                  'Time: {:.6f} seconds'
+                  ''.format(res[0], res[1], res[2], t))
+            print('=' * 75)
+
+            print('Collecting garbage...', end=' ')
+            del res
+            collect()
+            print('DONE!\n\nSelection sort')
+            t = time()
+            res = selection(deepcopy(A))
+            t = time() - t
+            print('=' * 75)
+            print('Array: \n{}\n'
+                  'Comparisions: {} | Exchanges: {}\n'
+                  'Time: {:.6f} seconds'
+                  ''.format(res[0], res[1], res[2], t))
+            print('=' * 75)
+
+            print('Collecting garbage...', end=' ')
+            del res
+            collect()
+            print('DONE!\n\nInsertion sort')
+            t = time()
+            res = insertion(deepcopy(A))
+            t = time() - t
+            print('=' * 75)
+            print('Array: \n{}\n'
+                  'Comparisions: {} | Exchanges: {}\n'
+                  'Time: {:.6f} seconds'
+                  ''.format(res[0], res[1], res[2], t))
+            print('=' * 75)
+
+            print('Collecting garbage...', end=' ')
+            del res
+            collect()
+            print('DONE!\n\nCocktail shaker sort')
+            t = time()
+            res = shake(deepcopy(A))
+            t = time() - t
+            print('=' * 75)
+            print('Array: \n{}\n'
+                  'Comparisions: {} | Exchanges: {}\n'
+                  'Time: {:.6f} seconds'
+                  ''.format(res[0], res[1], res[2], t))
+            print('=' * 75)
+
+            print('Collecting garbage...', end=' ')
+            del res
+            collect()
+            print('DONE!\n\nShellsort')
+            gp = shellprep(len(A))
+            t = time()
+            res = shell(deepcopy(A), gp)
+            t = time() - t
+            print('=' * 75)
+            print('Array: \n{}\n'
+                  'Comparisions: {} | Exchanges: {}\n'
+                  'Time: {:.6f} seconds'
+                  ''.format(res[0], res[1], res[2], t))
+            print('=' * 75)
+
+            print('Collecting garbage...', end=' ')
+            del res, gp
+            collect()
+            print('DONE!\n\nHeapsort')
+            t = time()
+            res = heap(deepcopy(A))
+            t = time() - t
+            print('=' * 75)
+            print('Array: \n{}\n'
+                  'Comparisions: {} | Exchanges: {}\n'
+                  'Time: {:.6f} seconds'
+                  ''.format(res[0], res[1], res[2], t))
+            print('=' * 75)
+
+            print('Collecting garbage...', end=' ')
+            del res
+            collect()
+            print('DONE!')
+            pass
         else:
             print('Error! Choose a number of the algorithm.')
             continue
