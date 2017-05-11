ed4843d9f5d067b15cf78913ebc1b2b9f4168fd9
notemissary <emissarys@protonmail.com>
2017-05-11 10:13:49 +0300

    Ah

---
+++ Lab11/7.py
@@ -0,0 +1,40 @@
+import os
+# os.chdir('D:/PyHub')
+# os.chdir('D:/PyHub/AP-Labs')
+# os.chdir('D:/PyHub/AP-Labs/Lab11')
+os.chdir('D:/PyHub/AP-Labs/Lab11/Anketa')
+print('We\'re here now: {}'.format(os.getcwd()))
+ans = []
+while True:
+    name = input('What\'s your name?\n'
+                 '> ')
+    if not (name.isalpha() and name.istitle()):
+        print('Please input name!')
+        continue
+    age = input('How old are you?\n'
+                 '> ')
+    if int(age) not in range(10, 101):
+        print('You\'re too young or too old! Call someone else to take the '
+              'test :)')
+        continue
+    phone = input('Your phone number?\n'
+                  '> ')
+    if not phone.startswith('+') or len(phone) != 13:
+        print('Number must start with + and has length of 13!')
+        continue
+    email = input('Your e-mail?\n'
+                  '> ')
+    if not ('@' and '.') in email:
+        print('Please, provide an actual email address!')
+        continue
+    with open('Anket.txt', 'w') as g:
+        g.write('Имя: ' + name + '\n')
+        g.write('Возраст: ' + age + '\n')
+        g.write('Моб. номер: ' + phone + '\n')
+        g.write('Электроннный адрес: ' + email + '\n')
+    with open('Anket.txt') as f:
+        line = f.readlines()
+    for i in line:
+        print(i, sep='')
+    if input('Press Enter to continue...') != '':
+        break
--- Lab11/b.py
+++ Lab11/b.py
@@ -1,2 +1,17 @@
 # Дыма Владимир. КНИТ16-А
+from string import ascii_letters
+from random import shuffle
+b = []
+for i in ascii_letters:
+    b.append(i)
+shuffle(b)
 
+f = open('in.txt', 'w')
+for i in b:
+    f.write(i)
+f.close()
+
+with open("in.txt") as f:
+    with open("out.txt", "w") as f1:
+        for line in f:
+            f1.write(line)
